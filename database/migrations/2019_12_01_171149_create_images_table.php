575c0ddb9dec96cf46b5ee4780e4740ff34ebc08
Hailot <lordfe@kreakageklubben.org>
2019-12-01 18:20:14 +0100

    init

---
+++ database/migrations/2019_12_01_171149_create_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('path');
+            $table->double('size', 8, 2)->default(0);
+            $table->unsignedInteger('auth_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
