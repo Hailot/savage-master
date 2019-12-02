2fdf419faa609235203e6e957cb6718465db71e1
Hailot <lordfe@kreakageklubben.org>
2019-12-02 18:54:21 +0100

    ui stuff

--- resources/views/spells/browse.blade.php
+++ resources/views/spells/browse.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-sm-12">
                 <div class="card">
-                    <div class="card-header">All Gear</div>
+                    <div class="card-header">All Spells</div>
                     <div class="card-body">
                         <div class="table-responsive">
                             <table class="table table-striped">
