fe5e8854b9ce565720eb9e353586d84df29a4d5b
Hailot <lordfe@kreakageklubben.org>
2019-12-02 17:34:10 +0100

    form responsive fix

--- resources/views/gear/browse.blade.php
+++ resources/views/gear/browse.blade.php
@@ -3,10 +3,11 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-12">
+            <div class="col-sm-12">
                 <div class="card">
                     <div class="card-header">All Gear</div>
                     <div class="card-body">
+                        <div class="table-responsive">
                         <table class="table table-striped">
                             <tr>
                                 <th>pic</th>
@@ -20,7 +21,7 @@
                             </tr>
                             @foreach ($gears as $gear)
                                 <tr>
-                                    <th><img width="100px" src="{{$gear->url}}"></th>
+                                    <th><img class="img-thumbnail" src="{{$gear->url}}"></th>
                                     <td>{{$gear->name}}</td>
                                     <td>{{$gear->description}}</td>
                                     <td>{{$gear->damage}}</td>
@@ -31,6 +32,7 @@
                                 </tr>
                             @endforeach
                         </table>
+                            <div class="table-responsive">
                     </div>
                 </div>
             </div>
