ac2ffa8ec0b91ec9c086292c908416bde9ad1a21
Jesper Carlsen <jc@lendme.dk>
2019-12-06 15:30:05 +0100

    started work on custom json data files

--- database/seeds/CreaturesTableSeeder.php
+++ database/seeds/CreaturesTableSeeder.php
@@ -11,6 +11,28 @@ class CreaturesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('creatures')->delete();
+
+        $data = new ArrayObject();
+
+        $gearjson = File::get("database/json/creatures/monsterSRD.json");
+        $gear = json_decode($gearjson);
+        foreach ($gear as $item) {
+            $data->offsetSet(null, $item);
+        }
+
+        foreach ($data as $gear)
+        {
+            Gear::create(array(
+                'name' => $gear->gear_name,
+                'description' => '',
+                'damage' => $gear->gear_damage,
+                'cost' => $gear->gear_cost,
+                'weight' => $gear->gear_weight,
+                'properties' => $gear->gear_properties,
+                'type' => $gear->gear_type,
+                'pic' => $gear->gear_pic
+            ));
+        }
     }
 }
